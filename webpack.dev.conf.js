99bdf44ae1d84627b79433db9e7b6b165390c64d
Lee Goddard <leegee@gmail.com>
2018-11-13 16:17:18 +0100

    Added Webpack

---
+++ webpack.dev.conf.js
@@ -0,0 +1,84 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:8080',
+      './src/shop-app.js'
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, '..', 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      // {
+      //   test: /\.txt$/,
+      //   use: 'raw-loader'
+      // },
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js', '.html' // , '.txt'
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      verbose: true,
+      root: path.resolve(__dirname)
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyWebpackPlugin([
+      // {
+      //   from: path.resolve(__dirname, './static'),
+      //   to: 'static'
+      // },
+      {
+        from: path.resolve(__dirname, './manifest.json'),
+        to: '.'
+      },
+      {
+        from: path.resolve(__dirname, './images'),
+        to: 'images'
+      },
+      {
+        from: path.resolve(__dirname, './data'),
+        to: 'data'
+      },
+      {
+        from: path.join(
+          path.resolve(__dirname, './node_modules/@webcomponents/webcomponentsjs/'),
+          '*.js'
+        ),
+        to: './webcomponentjs',
+        flatten: true
+      }
+    ]),
+    new webpack.IgnorePlugin(/vertx/),
+    new webpack.HotModuleReplacementPlugin(),
+  ]
+};
