69e12e878e8e228b643f7d0d1e9eca75e387b912
Lee Goddard <leegee@gmail.com>
2020-04-02 10:54:59 +0200

    Remove cruft URLs

--- src/Config.js
+++ src/Config.js
@@ -9,7 +9,6 @@ if (!process.env.currencyconverterapikey) {
 }
 
 export class Config {
-
     static get defaultlanguage() {
         return 'en';
     }
@@ -68,15 +67,13 @@ export class Config {
             {
                 name: 'mens_outerwear',
                 sheetName: 'mens_outerwear',
-                url: 'data/mens_outerwear.json',
-                title: 'Men\'s Outerwear',
+                title: 'Men\'s Clothes',
                 image: 'images/mens_outerwear.jpg',
                 placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXAABAQEAAAAAAAAAAAAAAAAAAAIEAQEAAAAAAAAAAAAAAAAAAAACEAAAAwYHAQAAAAAAAAAAAAAAERMBAhIyYhQhkaEDIwUVNREBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8A3dkr5e8tfpwuneJITOzIcmQpit037Bw4mnCVNOpAAQv/2Q=='
             },
             {
                 name: 'ladies_outerwear',
                 sheetName: 'mens_outerwear',
-                url: 'data/ladies_outerwear.json',
                 title: 'Ladies\' Outerwear',
                 image: 'images/ladies_outerwear.jpg',
                 placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAWQABAQAAAAAAAAAAAAAAAAAAAAEBAQEAAAAAAAAAAAAAAAAAAAIDEAABAwMFAQAAAAAAAAAAAAARAAEygRIDIlITMwUVEQEBAAAAAAAAAAAAAAAAAAAAQf/aAAwDAQACEQMRAD8Avqn5meQ0kwk1UyclmLtNj7L4PQoioFf/2Q=='
@@ -84,7 +81,6 @@ export class Config {
             {
                 name: 'mens_tshirts',
                 sheetName: 'mens_outerwear',
-                url: 'data/mens_tshirts.json',
                 title: 'Men\'s T-Shirts',
                 image: 'images/mens_tshirts.jpg',
                 placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAWwABAQEAAAAAAAAAAAAAAAAAAAMEAQEAAAAAAAAAAAAAAAAAAAAAEAABAwEJAAAAAAAAAAAAAAARAAESEyFhodEygjMUBREAAwAAAAAAAAAAAAAAAAAAAEFC/9oADAMBAAIRAxEAPwDb7kupZU1MTGnvOCgxpvzEXTyRElCmf//Z'
@@ -92,7 +88,6 @@ export class Config {
             {
                 name: 'ladies_tshirts',
                 sheetName: 'mens_outerwear',
-                url: 'data/ladies_tshirts.json',
                 title: 'Ladies\' T-Shirts',
                 image: 'images/ladies_tshirts.jpg',
                 placeholder: 'data:image/jpeg;base64,/9j/4QAYRXhpZgAASUkqAAgAAAAAAAAAAAAAAP/sABFEdWNreQABAAQAAAAeAAD/7gAOQWRvYmUAZMAAAAAB/9sAhAAQCwsLDAsQDAwQFw8NDxcbFBAQFBsfFxcXFxcfHhcaGhoaFx4eIyUnJSMeLy8zMy8vQEBAQEBAQEBAQEBAQEBAAREPDxETERUSEhUUERQRFBoUFhYUGiYaGhwaGiYwIx4eHh4jMCsuJycnLis1NTAwNTVAQD9AQEBAQEBAQEBAQED/wAARCAADAA4DASIAAhEBAxEB/8QAXwABAQEAAAAAAAAAAAAAAAAAAAMFAQEBAAAAAAAAAAAAAAAAAAABAhAAAQIDCQAAAAAAAAAAAAAAEQABITETYZECEjJCAzMVEQACAwAAAAAAAAAAAAAAAAAAATFBgf/aAAwDAQACEQMRAD8AzeADAZiFc5J7BC9Scek3VrtooilSNaf/2Q=='
@@ -100,9 +95,7 @@ export class Config {
         ];
     }
 
-    // https://docs.google.com/spreadsheets/d/12R_GOM47f9rgvIDveaZkfRiwZjUMHBicbIzXVIotDPs/edit#gid=0
     // https://sheets.googleapis.com/v4/spreadsheets/SPREADSHEET_ID/values/RANGE?key=apiKey
-    // https://sheets.googleapis.com/v4/spreadsheets/12R_GOM47f9rgvIDveaZkfRiwZjUMHBicbIzXVIotDPs/values/mens_outerwear?key=AIzaSyD2mfHeSMho_JqZYvBbNvkdqr3gumGQsWk
     // https://developers.google.com/sheets/api/guides/concepts
     static getGoogleSheetsUrlForSheetName(sheetName) {
         return 'https://sheets.googleapis.com/v4/spreadsheets/'
@@ -115,7 +108,11 @@ export class Config {
         return 'foo-bar' || undefined;
     }
 
-    static get payPalCientId() {
+    static get payPalSandboxClientId() {
         return process.env.ppclientid;
+    }
+
+    static get useSandbox(){
+        return true;
     }
 }
